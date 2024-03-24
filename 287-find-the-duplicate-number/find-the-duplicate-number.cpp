e0c2527e502391284acc7f70047cf83ef7e040d3
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-03-25 00:39:08 +0530

    Update find-the-duplicate-number.cpp

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -20,3 +20,22 @@ public:
         return slow;
     }
 };
+
+
+//second approach using maps
+
+class Solution {
+public:
+    int findDuplicate(std::vector<int>& nums) {
+        map<int,int> mp;
+        for(int i:nums){
+            mp[i]++;
+        }
+        for(auto i:mp){
+            if(i.second>1){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
