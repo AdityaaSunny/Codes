b52ce753e3014647ecc875c2f010367cc31dcc5d
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-04-06 02:43:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (58.28%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
-    bool check(char x, char y) {
-        return abs(x - y) == 32;
-    }
-
     string makeGood(string s) {
-        string res = "";
-        for (char c : s) {
-            if (!res.empty() && check(res.back(), c)) res.pop_back();
-            else res += c;
+         stack<char> st;
+        st.push(s[0]);
+        for(int i=1;i<s.size();i++)
+        {
+            if(!st.empty() && st.top()+32 == s[i])
+            {
+                st.pop();
+            }
+            else if(!st.empty() && st.top() == s[i]+32)
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
         }
-        return res;
+        s.clear();
+        while(!st.empty())
+        {
+            s.push_back(st.top());
+            st.pop();
+        }
+        reverse(s.begin(),s.end());
+        return s;
     }
 };
