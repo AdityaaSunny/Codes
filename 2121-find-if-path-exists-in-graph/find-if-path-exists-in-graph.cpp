cba5dac5a1067ab959713856b7a4c9702539bbc5
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-04-22 00:46:06 +0530

    Time: 219 ms (99.17%) | Memory: 125.4 MB (88.42%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,30 +1,31 @@
 class Solution {
+    vector<int> leader;
+    vector<int> size;
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int, vector<int>> graph;
-        for (const auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-        }
-        
-        unordered_set<int> visited;
-        return dfs(source, destination, graph, visited);
+    int find(int u) {
+        while (u != leader[u])
+            u = leader[u];
+        return u;
     }
-    
-    bool dfs(int node, int destination, unordered_map<int, vector<int>>& graph, unordered_set<int>& visited) {
-        if (node == destination) {
-            return true;
-        }
-        visited.insert(node);
-        for (int neighbor : graph[node]) {
-            if (visited.find(neighbor) == visited.end()) {
-                if (dfs(neighbor, destination, graph, visited)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+    void combine(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u==v)
+            return;
+        if (size[u]>size[v])
+            swap(u, v);
+        leader[u] = v;
+        size[v] += size[u];
+    }
+    bool validPath(const int n, const vector<vector<int>>& edges, const int source, const int destination) {
+        ios::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
+        leader = vector<int>(n);
+        size = vector<int>(n, 1);
+        for (int i=0; i<n; i++)
+            leader[i] = i;
+        for (const auto& edge : edges)
+            combine(edge[0], edge[1]);
+        return find(source)==find(destination);
     }
 };
