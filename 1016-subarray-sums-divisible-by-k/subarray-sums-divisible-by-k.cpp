f085e29296d4cec144439fc8d3d6fbe64e6810d2
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-06-09 14:22:24 +0530

    Time: 30 ms (89.17%) | Memory: 32 MB (99.95%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -2,28 +2,17 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         
-    int sum=0;
-    int count=0;
-    map<int,int>mp;
-    mp[0]++;                  // always one prefix sum that is initially 0
-
-    
-    for(int i=0;i<nums.size();i++)
-    {
-        sum+=nums[i];
-        int rem=sum%k;        //Calculte the remainders of current sun
-        
-        if(rem<0)             // Handle negative remainders to ensure they are positive
+     int res = 0, mp[k];
+        memset(mp, 0, sizeof(mp));
+        mp[0] = 1;
+        int sum = 0;
+        for(auto& num : nums)
         {
-            rem=k+rem;
+            sum += num;
+            int rem = (sum % k + k) % k;
+            res += mp[rem]++;
         }
-        
-        if(mp[rem]>0)          // If remainder has seen before, it means there are subarrays
-            count+=mp[rem];    // which sum to multiple of k. Add the count of occurrences
-
-        mp[rem]++;              // Increment the count for this remainder in the map
-    }
-    return count;
+        return res;
         
     }
 };
