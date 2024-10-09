d2816004831b4c67d23feed13bccc71ff3f20b99
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-10-10 04:53:47 +0530

    Time: 101 ms (58.31%) | Memory: 35.2 MB (20.65%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        int lb=0;
-        int moves=0;
-        for(int i=0; i<s.size(); i++)
-        {
-            if(s[i]==']')
-            {
-                if(lb==0)
-                {
-                    moves++;
-                    lb++;
-                }
-                else
-                {
-                    lb--;
-                }
-            }
-            else
-            {
-                lb++;
+        stack<char> st;
+        
+        for (auto i : s) {
+            if (i == '[') {
+                st.push(i); 
+            } else {
+                if (!st.empty() && st.top() == '[') {
+                    st.pop(); 
+                } 
             }
         }
-
-        return moves;
+        return (st.size()+1)/2;
     }
 };
