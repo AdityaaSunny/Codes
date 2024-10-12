ec3a615605f31e495f0a883fbb041356ce571541
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-10-12 09:16:01 +0530

    Time: 14 ms (13.68%) | Memory: 23 MB (5.96%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         set<int,greater<int>> ans;
         for(int i=0;i<nums.size();i++) {
             ans.insert(nums[i]);
