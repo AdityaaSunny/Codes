64a9183ef9a7f6b27f4ff5315f163af01576c592
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-10-15 16:43:20 +0530

    Time: 64 ms (99.06%) | Memory: 112.8 MB (7.94%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
         int currsum=0;
         int y=0;
         int j=k-1;
