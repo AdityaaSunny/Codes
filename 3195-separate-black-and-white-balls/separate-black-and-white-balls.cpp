ec188aa49b3a29687e95e26fd67bdbc637d39808
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-10-16 14:04:01 +0530

    Time: 36 ms (30.89%) | Memory: 15.7 MB (44.92%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        long long swaps=0;
         int black=0;
+        long long swap=0;
         for(int i=0;i<s.length();i++){
             if(s[i]=='0'){
-                swaps +=black;
+                swap+=black;
             }else{
                 black++;
             }
         }
-        return swaps;
+        return swap;
     }
 };
