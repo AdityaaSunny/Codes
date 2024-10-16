7ea513dea5580128dd882438b59152c1aa8dc4b1
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-10-16 14:14:27 +0530

    Time: 3 ms (43.32%) | Memory: 7.7 MB (78.90%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -1,16 +1,44 @@
 class Solution {
 public:
-    string longestDiverseString(int a, int b, int c, char aa = 'a', char bb = 'b', char cc = 'c') {
-        if (a < b)
-            return longestDiverseString(b, a, c, bb, aa, cc);
-        if (b < c)  
-            return longestDiverseString(a, c, b, aa, cc, bb);
+    string longestDiverseString(int a, int b, int c) {
+        // Priority queue to store the characters and their counts.
+        priority_queue<pair<int, char>> pq;
+        if (a > 0) pq.push({a, 'a'});
+        if (b > 0) pq.push({b, 'b'});
+        if (c > 0) pq.push({c, 'c'});
 
-        if (b == 0)
-            return string(min(2, a), aa);
-        else {
-            int use_a = min(2, a), use_b = (a - use_a >= b); 
-            return string(use_a, aa) +  string(use_b, bb) + longestDiverseString(a - use_a, b - use_b, c, aa, bb, cc);
-        } 
+        string result = "";
+
+        while (!pq.empty()) {
+            // Get the character with the most count.
+            auto [count1, char1] = pq.top();
+            pq.pop();
+
+            // Check if the last two characters in result are the same.
+            if (result.size() >= 2 && result.back() == char1 && result[result.size() - 2] == char1) {
+                if (pq.empty()) break;  // No valid characters left.
+
+                // Get the second most character.
+                auto [count2, char2] = pq.top();
+                pq.pop();
+
+                // Add the second character to avoid consecutive repetition.
+                result += char2;
+                count2--;
+
+                if (count2 > 0) pq.push({count2, char2});
+
+                // Push back the most frequent character for later use.
+                pq.push({count1, char1});
+            } else {
+                // If no repetition issue, add the most frequent character.
+                result += char1;
+                count1--;
+
+                if (count1 > 0) pq.push({count1, char1});
+            }
+        }
+
+        return result;
     }
 };
